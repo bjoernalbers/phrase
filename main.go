5e39a86b5fdea5cefdcb966989735916b52d6ba8
Björn Albers <bjoernalbers@gmail.com>
2024-01-06 16:03:26 +0100

    Return shuffled example passphrase.

--- main.go
+++ main.go
@@ -1,8 +1,31 @@
 // phrase - the passphrase generator
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+)
+
+var words = []string{
+	"correct",
+	"horse",
+	"battery",
+	"staple",
+}
 
 func main() {
-	fmt.Println("correct horse battery staple")
+	randomWords, _ := pick(words, 4)
+	fmt.Println(strings.Join(randomWords, " "))
+}
+
+// pick returns a slice of n random words from words.
+func pick(words []string, n int) (randomWords []string, err error) {
+	if n <= 0 {
+		return nil, fmt.Errorf("Number of words to pick must be greater than 0")
+	}
+	for i := 0; i < n; i++ {
+		randomWords = append(randomWords, words[rand.Intn(len(words))])
+	}
+	return randomWords, nil
 }
