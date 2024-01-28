b40370dddf364ebd6c05f9da625bbe6bb96d7dab
Björn Albers <bjoernalbers@gmail.com>
2024-01-28 20:05:01 +0100

    Add lists.ValidateWord() function.

--- lists/lists.go
+++ lists/lists.go
@@ -1,6 +1,13 @@
 // Package lists provides lists of words to generate passphrases from.
 package lists
 
+import (
+	"fmt"
+	"regexp"
+)
+
+const ValidWordRegexp = `\A[a-z]{3,9}\z`
+
 // Lists contains all wordlists grouped by two-letter language code.
 var Lists map[string][]string
 
@@ -9,4 +16,11 @@ func init() {
 
 	// TODO: Replace fake data with real wordlist.
 	Lists[""] = []string{"correct", "horse", "battery", "staple"}
+}
+
+func ValidateWord(word string) error {
+	if matched, _ := regexp.MatchString(ValidWordRegexp, word); !matched {
+		return fmt.Errorf("invalid word: %#v", word)
+	}
+	return nil
 }
---
+++ lists/lists_test.go
@@ -0,0 +1,32 @@
+package lists
+
+import "testing"
+
+func TestValidateWord(t *testing.T) {
+	tests := []struct {
+		name    string
+		word    string
+		wantErr bool
+	}{
+		{"two letters", "go", true},
+		{"ten letters", "gophergoph", true},
+		{"contains space", "go pher", true},
+		{"contains umlaut", "göpher", true},
+		{"contains hyphen", "go-pher", true},
+		{"contains dot", "go.pher", true},
+		{"contains digit", "goph3r", true},
+		{"all letters uppercase", "GOPHER", true},
+		{"first letter capitalized", "Gopher", true},
+		{"last letter capitalized", "gopheR", true},
+		{"all letters lowercase", "gopher", false},
+		{"three letters", "gop", false},
+		{"nine letters", "gophergop", false},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			if err := ValidateWord(test.word); (err != nil) != test.wantErr {
+				t.Fatalf("validateWord(%#v) = %v, wantErr: %#v\n", test.word, err, test.wantErr)
+			}
+		})
+	}
+}
