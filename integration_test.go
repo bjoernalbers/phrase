fa0a575aefa01258c7119b6b6873046d04cf037a
Björn Albers <bjoernalbers@gmail.com>
2024-04-19 12:05:52 +0200

    Fix test regexp for passphrase with digits.

--- integration_test.go
+++ integration_test.go
@@ -91,7 +91,7 @@ func TestIntegration(t *testing.T) {
 		{"change number of words", []string{"-w", "3", "-f", "testdata/gopher.txt"}, `^gopher gopher gopher\n$`, false},
 		{"change number of passphrases", []string{"-p", "3", "-w", "1", "-f", "testdata/gopher.txt"}, `^gopher\ngopher\ngopher\n$`, false},
 		{"change separator", []string{"-s", "-", "-f", "testdata/gopher.txt"}, `^gopher-gopher-gopher-gopher\n$`, false},
-		{"add digits", []string{"-d", "3"}, `\W[0-9]{3}\W`, false},
+		{"add digits", []string{"-d", "3"}, `[0-9]{3}`, false},
 		{"capitalize words", []string{"-C", "-f", "testdata/gopher.txt"}, `^Gopher Gopher Gopher Gopher\n$`, false},
 		{"missing wordlist", []string{"-f", "this-file-does-not-exist"}, `this-file-does-not-exist`, true},
 	}
